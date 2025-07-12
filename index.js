5ef070414753422bbe654ca60d004caf1d370d81
Santiago Villabona <villabonasantiago@gmail.com>
2025-07-11 20:25:37 -0400

    se instalaron todas las dependencias

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require("cors")
+
+app.use(express.json())
+app.use(cors())
+
 app.listen(3000, console.log("¡Servidor encendido!"))
+
 app.get("/home", (req, res) => {
     res.send("Hello World Express Js")
 })
+
